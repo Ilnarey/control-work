8fcf70e5db42102c83d86ef69a3d83af2dc3dccb
Ilnarey <163994910+Ilnarey@users.noreply.github.com>
2024-12-18 15:32:01 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import { FinanceModel } from "src/model/model.js";
-import { FinanceView } from "src/view/view.js";
-import { FinancePresenter } from "src/presenter/presenter.js";
+import { FinanceModel } from "/src/model/model.js";
+import { FinanceView } from "/src/view/view.js";
+import { FinancePresenter } from "/src/presenter/presenter.js";
 
 // Создаем экземпляры модели, представления и презентера
 const model = new FinanceModel();
